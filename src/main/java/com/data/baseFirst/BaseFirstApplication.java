699bbba6a6fb23544432a8cc62ae3c2351e06e86
Carlos Krefer <carloskrefer@hotmail.com>
2024-10-22 11:03:06 -0300

    commit

---
+++ src/main/java/com/data/baseFirst/BaseFirstApplication.java
@@ -0,0 +1,13 @@
+package com.data.baseFirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data/baseFirst/BaseFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.baseFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
